79d83ef04a31aa227a203aaa9345b6e12a4a8620
Jakub Lidke <jakub.lidke@uni-marburg.de>
2023-07-27 12:18:17 +0200

    fix: if no prefix is set, `_` is also not added as prefix to patient pseudonym.

--- src/main/kotlin/dev/dnpm/etl/processor/pseudonym/PseudonymizeService.kt
+++ src/main/kotlin/dev/dnpm/etl/processor/pseudonym/PseudonymizeService.kt
@@ -19,6 +19,7 @@
 
 package dev.dnpm.etl.processor.pseudonym
 
+import ca.uhn.fhir.util.StringUtil
 import de.ukw.ccc.bwhc.dto.MtbFile
 import dev.dnpm.etl.processor.config.PseudonymizeConfigProperties
 
@@ -28,7 +29,9 @@ class PseudonymizeService(
 ) {
 
     fun pseudonymize(mtbFile: MtbFile): MtbFile {
-        val patientPseudonym = "${configProperties.prefix}_${generator.generate(mtbFile.patient.id)}"
+        val patientPseudonym: String = if(configProperties.prefix.isNotEmpty())
+            "${configProperties.prefix}_${generator.generate(mtbFile.patient.id)}"
+        else generator.generate(mtbFile.patient.id)
 
         mtbFile.episode.patient = patientPseudonym
         mtbFile.carePlans.forEach { it.patient = patientPseudonym }
