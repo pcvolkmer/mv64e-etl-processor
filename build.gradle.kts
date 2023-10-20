0305e69e9ee56fd4303c8d79c68983025ce521f9
Paul-Christian Volkmer <volkmer_p@ukw.de>
2023-10-20 13:49:38 +0200

    chore: Update Spring Boot to version 3.1.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     war
-    id("org.springframework.boot") version "3.1.4"
+    id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.9.10"
     kotlin("plugin.spring") version "1.9.10"
