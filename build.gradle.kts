d5552b3ca4e1fbd423b365dcbeb08e53a7e19633
Paul-Christian Volkmer <volkmer_p@ukw.de>
2023-11-21 08:31:18 +0100

    chore: Update Kotlin version to 1.9.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
     war
     id("org.springframework.boot") version "3.1.5"
     id("io.spring.dependency-management") version "1.1.3"
-    kotlin("jvm") version "1.9.10"
-    kotlin("plugin.spring") version "1.9.10"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.spring") version "1.9.20"
 }
 
 group = "de.ukw.ccc"
