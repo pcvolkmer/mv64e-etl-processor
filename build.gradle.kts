acf49a892ebada0cd2259918ab6fee1bf9cdbf62
Paul-Christian Volkmer <volkmer_p@ukw.de>
2023-12-04 14:37:58 +0100

    chore: update Kotlin and dependency management plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 plugins {
     war
     id("org.springframework.boot") version "3.1.6"
-    id("io.spring.dependency-management") version "1.1.3"
-    kotlin("jvm") version "1.9.20"
-    kotlin("plugin.spring") version "1.9.20"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
 }
 
 group = "de.ukw.ccc"
