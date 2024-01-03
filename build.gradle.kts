91f17f6af5ea75e6c5dfd7ae660cc34edbbe5e66
Paul-Christian Volkmer <volkmer_p@ukw.de>
2024-01-03 12:26:54 +0100

    chore: update mockito-kotlin test dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ var versions = mapOf(
     "bwhc-dto-java" to "0.2.0",
     "hapi-fhir" to "6.10.2",
     "httpclient5" to "5.2.1",
-    "mockito-kotlin" to "5.1.0"
+    "mockito-kotlin" to "5.2.1"
 )
 
 java {
