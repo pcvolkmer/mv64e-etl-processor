ca1e73a0b52462eedff946dc31c07713d805d340
Paul-Christian Volkmer <volkmer_p@ukw.de>
2024-05-16 17:59:22 +0200

    chore: update bwhc-dto-java dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ group = "de.ukw.ccc"
 version = "0.9-SNAPSHOT"
 
 var versions = mapOf(
-    "bwhc-dto-java" to "0.2.0",
+    "bwhc-dto-java" to "0.3.0",
     "hapi-fhir" to "6.10.5",
     "httpclient5" to "5.2.3",
     "mockito-kotlin" to "5.3.1",
