86b371611637cd0e5b2ba5034da67c05217e0e3b
Paul-Christian Volkmer <code@pcvolkmer.de>
2024-10-07 22:13:14 +0200

    chore: update to Spring Boot 3.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     war
-    id("org.springframework.boot") version "3.3.1"
+    id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.5"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
