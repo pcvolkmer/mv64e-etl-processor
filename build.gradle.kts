ff27b7157d4d032626e74bf154be5241261b92bd
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-02-10 19:18:27 +0100

    chore: update spring boot and dependency management plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     war
-    id("org.springframework.boot") version "3.3.7"
-    id("io.spring.dependency-management") version "1.1.6"
+    id("org.springframework.boot") version "3.3.8"
+    id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
     jacoco
