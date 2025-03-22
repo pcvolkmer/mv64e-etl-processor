9bdd8ba3751a88d3530d803318da1d5e4ecb9219
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-03-22 10:20:52 +0100

    chore: update Spring Boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     war
-    id("org.springframework.boot") version "3.3.9"
+    id("org.springframework.boot") version "3.3.10"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
