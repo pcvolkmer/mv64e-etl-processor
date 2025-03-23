10f7e917fcd281ecf7bde779f65e8313f9de02dd
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-03-23 11:50:13 +0100

    refactor: ignore unused exception

--- src/main/kotlin/dev/dnpm/etl/processor/monitoring/ConnectionCheckService.kt
+++ src/main/kotlin/dev/dnpm/etl/processor/monitoring/ConnectionCheckService.kt
@@ -88,7 +88,7 @@ class KafkaConnectionCheckService(
                 Instant.now(),
                 if (result.available == available) { result.lastChange } else { Instant.now() }
             )
-        } catch (e: TimeoutException) {
+        } catch (_: TimeoutException) {
             ConnectionCheckResult.KafkaConnectionCheckResult(
                 false,
                 Instant.now(),
@@ -138,7 +138,7 @@ class RestConnectionCheckService(
                 Instant.now(),
                 if (result.available == available) { result.lastChange } else { Instant.now() }
             )
-        } catch (e: Exception) {
+        } catch (_: Exception) {
             ConnectionCheckResult.RestConnectionCheckResult(
                 false,
                 Instant.now(),
@@ -191,7 +191,7 @@ class GPasConnectionCheckService(
                 Instant.now(),
                 if (result.available == available) { result.lastChange } else { Instant.now() }
             )
-        } catch (e: Exception) {
+        } catch (_: Exception) {
             ConnectionCheckResult.GPasConnectionCheckResult(
                 false,
                 Instant.now(),
