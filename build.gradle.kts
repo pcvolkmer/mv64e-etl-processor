17f4dc3512c922091041beb7c7bb84d467253e7c
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-06-08 20:02:16 +0200

    chore: update to Spring Boot 3.5.0 (#115)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
 
 plugins {
     war
-    id("org.springframework.boot") version "3.4.6"
+    id("org.springframework.boot") version "3.5.0"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
