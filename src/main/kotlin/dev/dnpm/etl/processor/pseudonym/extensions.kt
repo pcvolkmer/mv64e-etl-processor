70ff8e925aa79623bac043c77d5da71f209058d9
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-11-20 10:38:14 +0100

    fix: fix possible NPE for MSI patient refs

--- src/main/kotlin/dev/dnpm/etl/processor/pseudonym/extensions.kt
+++ src/main/kotlin/dev/dnpm/etl/processor/pseudonym/extensions.kt
@@ -87,7 +87,7 @@ infix fun Mtb.pseudonymizeWith(pseudonymizeService: PseudonymizeService) {
         it.patient?.id = patientPseudonym
     }
 
-    this.msiFindings?.forEach { it -> it.patient.id = patientPseudonym }
+  this.msiFindings?.forEach { it -> it.patient?.id = patientPseudonym }
 
     this.metadata?.researchConsents?.forEach { it ->
         val entry = it ?: return@forEach
