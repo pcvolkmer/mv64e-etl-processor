ec1b369e74e5a742a40d6c8bbdfb172310daa484
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-12-01 15:55:15 +0100

    fix: show error page
    This shows error page when trying to load
    unavailable resource.

--- src/main/kotlin/dev/dnpm/etl/processor/web/ApplicationControllerAdvice.kt
+++ src/main/kotlin/dev/dnpm/etl/processor/web/ApplicationControllerAdvice.kt
@@ -24,10 +24,11 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.servlet.resource.NoResourceFoundException
 
 @ControllerAdvice
 class ApplicationControllerAdvice {
-    @ExceptionHandler(NotFoundException::class)
+    @ExceptionHandler(NotFoundException::class, NoResourceFoundException::class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    fun handleNotFoundException(e: NotFoundException): String = "errors/404"
+    fun handleNotFoundException(e: Exception): String = "errors/404"
 }
