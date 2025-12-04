d9f48904ec33a15dcdfa55ca24f7a4a33686d905
Paul-Christian Volkmer <code@pcvolkmer.de>
2025-12-04 20:00:17 +0100

    chore: code formatting

--- src/main/java/dev/dnpm/etl/processor/consent/MiiBroadConsentEvaluator.java
+++ src/main/java/dev/dnpm/etl/processor/consent/MiiBroadConsentEvaluator.java
@@ -39,7 +39,7 @@ public class MiiBroadConsentEvaluator {
   private static final Logger log = LoggerFactory.getLogger(MiiBroadConsentEvaluator.class);
 
   private MiiBroadConsentEvaluator() {
-      // No content
+    // No content
   }
 
   /**
